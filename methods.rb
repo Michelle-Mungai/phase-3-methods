40d32f3daa3b253bd9e20e3c5191217664f3aa55
Michelle_Mungai <chellymungai@gmail.com>
2023-02-07 14:31:41 +0300

    Guard clause and methods work

--- methods.rb
+++ methods.rb
@@ -1,1 +1,30 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+greet("Naureen")
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+greet_with_default("Naureen")
+greet_with_default
+
+def add num1,num2
+    num1 + num2
+end
+puts add(2,5)
+
+def halve(number)
+    if number.class != Integer
+        return nil
+    end
+    number / 2
+end
+#     return nil unless number.class == Integer
+#     number / 2
+# end
